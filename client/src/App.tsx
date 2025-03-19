849856b655c8b4176ea3adb8b290eb14e8e15a20
gautamkumar1 <gautamraaz936@gmail.com>
2025-03-19 19:30:11 +0530

    frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p className="text-red-500">Test</p>
+      <Button variant="destructive">Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
