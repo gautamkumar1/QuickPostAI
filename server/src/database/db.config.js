7d6d59f31e3a92f31e908dac0e4273272b997e9d
gautamkumar1 <gautamraaz936@gmail.com>
2025-03-24 10:37:41 +0530

    Add advange node logger

--- server/src/database/db.config.js
+++ server/src/database/db.config.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient({
 async function connectDB() {
   try {
     await prisma.$connect();
-    console.log('✅ Database connected successfully!');
+    console.log('Database connected successfully');
   } catch (error) {
     console.error('❌ Error connecting to the database:', error);
   }
--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser"
 import { connectDB } from "./database/db.config.js";
 import userRoutes from "./routes/user-routes.js";
 import agentsRoutes from "./routes/agent-routes.js";
+import logger from "./../logger.js";
+import morgan from "morgan";
 import cors from "cors";
 dotenv.config()
 const app = express()
@@ -13,6 +15,23 @@ const corsOptions = {
   allowedHeaders: "Content-Type,Authorization", 
   credentials: true,
 };
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 app.use(cors(corsOptions));
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
