af4997d5b200d9eb21ceb25f41461f46e29de6ab
gautamkumar1 <gautamraaz936@gmail.com>
2025-03-27 17:00:23 +0530

    Feat: Auto schedule tweet

--- server/src/routes/agent-routes.js
+++ server/src/routes/agent-routes.js
@@ -2,10 +2,12 @@ import {Router} from "express";
 import { getTweetsDetails, tweetGenerate } from "../controllers/agent-controllers.js";
 import { generateLimiter } from "../controllers/rate-limit-controllers.js";
 import isAuthenticated from "../middleware/auth-middleware.js";
+import { autoScheduleTweets } from "../controllers/autoScheduledTweets.js";
 
 const router = Router();
 router.post("/tweetGenerate",isAuthenticated,generateLimiter,tweetGenerate)
 // test route
 // router.post("/tweetGenerate",isAuthenticated,tweetGenerate)
 router.get("/getTweetsDetails",isAuthenticated,getTweetsDetails)
+router.post("/autoSchedule",isAuthenticated,autoScheduleTweets)
 export default router
