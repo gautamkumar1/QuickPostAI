b5ebd7c13b8f0965e2451f6849817e791907c043
gautamkumar1 <gautamraaz936@gmail.com>
2025-04-01 15:11:51 +0530

    update

--- client/src/Api/api.ts
+++ client/src/Api/api.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 
 // const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const VITE_BACKEND_URL = import.meta.env.VITE_NODE_ENV === "production"
-? import.meta.env.VITE_PROD_URL
+? import.meta.env.VITE_PROD_URL 
 : import.meta.env.VITE_DEV_URL;
 const axiosInstance = axios.create({
   baseURL: VITE_BACKEND_URL,
