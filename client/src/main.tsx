2f8c1fad8d7ea10260928c2fed4108db8d7ac766
gautamkumar1 <gautamraaz936@gmail.com>
2025-04-19 15:21:44 +0530

    Added vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,25 +5,18 @@ import App from './App.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from 'sonner'
-import { PostHogProvider} from 'posthog-js/react'
+import { Analytics } from "@vercel/analytics/react"
 const queryClient = new QueryClient();
 document.documentElement.classList.add("dark");
-const options = {
-  api_host: import.meta.env.VITE_APP_PUBLIC_POSTHOG_HOST,
-}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PostHogProvider
-    apiKey={import.meta.env.VITE_APP_PUBLIC_POSTHOG_KEY}
-    options={options}
-    >
-    <QueryClientProvider client={queryClient}>
+     <QueryClientProvider client={queryClient}>
     <ThemeProvider  defaultTheme="dark" storageKey="vite-ui-theme">
     <App />
     <Toaster position="bottom-right" richColors />
+    <Analytics />
     </ThemeProvider>
     </QueryClientProvider>
-    </PostHogProvider>
     
   </StrictMode>,
 )
